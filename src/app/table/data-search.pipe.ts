b4938d8e93969d7b93a74f96a1f839455276aa7f
Szymon Piecuch <szymek.piecuch@gmail.com>
2017-05-29 11:53:37 +0200

    Fixed pipes

--- src/app/table/data-search.pipe.ts
+++ src/app/table/data-search.pipe.ts
@@ -1,7 +1,8 @@
 import {Pipe, PipeTransform} from "@angular/core";
 
 @Pipe({
-  name: 'dataSearch'
+  name: 'dataSearch',
+  pure: false
 })
 
 export class DataSearchPipe implements PipeTransform {
--- src/app/table/data-sort.pipe.ts
+++ src/app/table/data-sort.pipe.ts
@@ -1,7 +1,8 @@
 import {Pipe, PipeTransform} from "@angular/core";
 
 @Pipe({
-  name: 'dataSort'
+  name: 'dataSort',
+  pure: false
 })
 
 export class DataSortPipe implements PipeTransform {
